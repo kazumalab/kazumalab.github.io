f2731368f3966cdec9317492c25ec28cd26f9743
kazumalab <kazuma-arashi@hotmail.co.jp>
2021-01-01 23:47:40 +0900

    add: link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -11,6 +12,37 @@ export default function Home() {
 
       <main>
         <h1>kazumalab's page</h1>
+
+        <ul>
+          <li>
+            <Link href="https://kazumalab.hatenablog.com/">
+              <a target="_blank">
+                Blog
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="https://twitter.com/kazumalab">
+              <a target="_blank">
+                Twitter
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="https://github.com/kazumalab" target="_blank">
+              <a target="_blank">
+                Github
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="https://www.npmjs.com/~kazumalab" target="_blank">
+              <a target="_blank">
+                npm
+              </a>
+            </Link>
+          </li>
+        </ul>
       </main>
 
       <footer className={styles.footer}>
