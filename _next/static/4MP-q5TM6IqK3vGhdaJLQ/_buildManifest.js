67226b9a78967d75b746eb0563b0f12a264c1ddb
kazumalab <kazumalab@users.noreply.github.com>
2021-01-03 13:59:42 +0000

    deploy: ed569e686271a1a6d517374836b38c8cc328813d

--- _next/static/4MP-q5TM6IqK3vGhdaJLQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc616615b7548d7244763.css","static\u002Fchunks\u002Fpages\u002Findex-e9060037bbded85f1104.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-96c65f745f1bef495e96.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
